2832caa271049cd38891a7eed388862a11c833a1
heng9544 <47099395+heng9544@users.noreply.github.com>
2019-01-31 17:11:58 +0900

    Delete 4 'for'

--- Check_Pattern.py
+++ Check_Pattern.py
@@ -7,25 +7,21 @@ im=image.load()
 for i in range(0,50):
     for j in range(0,50):
         im[i,j]=(255,255,255)
+    for j in range(101, 150):
+        im[i, j] = (255, 255, 255)
 for i in range(101,150):
     for j in range(0,50):
         im[i,j]=(255,255,255)
-for i in range(51,100):
-    for j in range(51,100):
-        im[i,j]=(255,255,255)
-for i in range(151,200):
-    for j in range(51,100):
-        im[i,j]=(255,255,255)
-for i in range(0,50):
-    for j in range(101,150):
-        im[i,j]=(255,255,255)
-for i in range(101,150):
     for j in range(101,150):
         im[i,j]=(255,255,255)
 for i in range(51,100):
+    for j in range(51,100):
+        im[i,j]=(255,255,255)
     for j in range(151,200):
         im[i,j]=(255,255,255)
 for i in range(151,200):
+    for j in range(51,100):
+        im[i,j]=(255,255,255)
     for j in range(151,200):
         im[i,j]=(255,255,255)
 
