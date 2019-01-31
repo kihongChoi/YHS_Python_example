c4a6012751fff0a924de2e4a844787adedb0a6e5
heng9544 <47099395+heng9544@users.noreply.github.com>
2019-01-31 16:53:39 +0900

    Create Check_Pattern.py

---
+++ Check_Pattern.py
@@ -0,0 +1,33 @@
+import numpy
+from PIL import Image
+
+image=Image.new('RGB',(200,200))
+im=image.load()
+
+for i in range(0,50):
+    for j in range(0,50):
+        im[i,j]=(255,255,255)
+for i in range(101,150):
+    for j in range(0,50):
+        im[i,j]=(255,255,255)
+for i in range(51,100):
+    for j in range(51,100):
+        im[i,j]=(255,255,255)
+for i in range(151,200):
+    for j in range(51,100):
+        im[i,j]=(255,255,255)
+for i in range(0,50):
+    for j in range(101,150):
+        im[i,j]=(255,255,255)
+for i in range(101,150):
+    for j in range(101,150):
+        im[i,j]=(255,255,255)
+for i in range(51,100):
+    for j in range(151,200):
+        im[i,j]=(255,255,255)
+for i in range(151,200):
+    for j in range(151,200):
+        im[i,j]=(255,255,255)
+
+image.save('test.png')
+image.show('test.png')
